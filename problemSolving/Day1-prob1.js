a28389de3e892509037ff5a89816c4d59a8a1cae
Majed Alqudah <Majedjameel062@gmail.com>
2025-10-18 21:18:18 +0300

    Ehm lmao

--- problemSolving/Day1-prob1.js
+++ problemSolving/Day1-prob1.js
@@ -2,8 +2,8 @@ const power =(pow)=>{
     return function(base){
         return pow**base
     }}
-let square = power(3)
+let square = power(2)
 let cube = power(3)
 
-console.log(square(2))  //9
+console.log(square(3))  //9
 console.log(cube(3))    //27
